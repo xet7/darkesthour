03c751b3650aad215e0a22831849159228b51029
Lauri Ojansivu <x@xet7.org>
2022-12-19 00:00:35 +0200

    Added scripts to add Amiga compile deps.

---
+++ Source/install-amiga.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -L /usr/local/bin/68k-amigaos-gcc ] ; then
+	(sudo apt-get -y install gcc-multilib)
+	(sudo cp archive/m68k-amigaos_linux_i386.tar.gz /tmp/)
+	(cd /opt && sudo tar zxf /tmp/m68k-amigaos_linux_i386.tar.gz)
+	(cd /usr/local/bin && sudo ln -s /opt/m68k-amigaos/bin/*m68k* . && sudo ln -s /opt/m68k-amigaos/bin/v* .)
+	(cd /usr/local/bin && sudo ln -s /opt/m68k-amigaos/bin/v* .)
+fi
