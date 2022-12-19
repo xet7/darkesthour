53f40fbf8602bcd1064e749c33b73cf50cea3632
Lauri Ojansivu <x@xet7.org>
2022-12-19 04:08:17 +0200

    Added more to menu.

--- Source/menu.sh
+++ Source/menu.sh
@@ -17,7 +17,7 @@ function pause(){
 echo "Darkest Hour: Build or run with dosbox/wine/qemu"
 echo
 PS3='Please enter your choice: '
-options=("List filetypes of built executeables" "Build All" "Run DOS" "Run Win32" "Run Win64" "Run Linux-x32" "Run Linux-x64" "Run Linux-arm32" "Run Linux-arm64" "Run Linux-m68k" "Run Linux-ppc" "Run Linux-ppc64" "Run Linux-ppc64le" "Run Linux-riscv64" "Run Linux-s390x" "Quit")
+options=("List filetypes of built executeables" "Build All" "Build AmigaOS3" "Build AppleII" "Build CBM-II" "Build C64" "Build DOS" "Build Plus4" "Build CommodorePET40columns" "Build CommodorePET80columns" "Build Plus4" "Build Linux-x32" "Build Linux-x64" "Build Linux-arm32" "Build Linux-arm64" "Build Linux-m68k" "Build Linux-riscv64" "Build Linux-ppc" "Build Linux-ppc64" "Build Linux-ppc64le" "Run DOS" "Run Win32" "Run Win64" "Run Linux-x32" "Run Linux-x64" "Run Linux-arm32" "Run Linux-arm64" "Run Linux-m68k" "Run Linux-ppc" "Run Linux-ppc64" "Run Linux-ppc64le" "Run Linux-riscv64" "Run Linux-s390x" "Quit")
 
 select opt in "${options[@]}"
 do
@@ -29,6 +29,91 @@ do
       ;;
     "Build All")
       make
+      break
+      ;;
+   "Build AmigaOS3")
+      make deps
+      make amigaos3
+      break
+      ;;
+   "Build AppleII")
+      make deps
+      make apple2
+      break
+      ;;
+   "Build C64")
+      make deps
+      make c64
+      break
+      ;;
+    "Build CBM-II")
+      make deps
+      make cbm
+      break
+      ;;
+    "Build CommodorePET40columns")
+      make deps
+      make pet40
+      break
+      ;;
+    "Build CommodorePET80columns")
+      make deps
+      make pet80
+      break
+      ;;
+    "Build DOS")
+      make deps
+      make dos
+      break
+      ;;
+    "Build Plus4")
+      make deps
+      make plus4
+      break
+      ;;
+    "Build Linux-x32")
+      make deps
+      make linux-x32
+      break
+      ;;
+   "Build Linux-x64")
+      make deps
+      make linux-x64
+      break
+      ;;
+    "Build Linux-arm32")
+      make deps
+      make linux-arm32
+      break
+      ;;
+    "Build Linux-arm64")
+      make deps
+      make linux-arm64
+      break
+      ;;
+    "Build Linux-m68k")
+      make deps
+      make linux-m68k
+      break
+      ;;
+    "Build Linux-riscv64")
+      make deps
+      make linux-riscv64
+      break
+      ;;
+    "Build Linux-ppc")
+      make deps
+      make linux-ppc
+      break
+      ;;
+    "Build Linux-ppc64")
+      make deps
+      make linux-ppc64
+      break
+      ;;
+    "Build Linux-ppc64le")
+      make deps
+      make linux-ppc64le
       break
       ;;
     "Run DOS")
