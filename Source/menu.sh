ce5c6240900fca0609ee8be37193ced713487f51
Lauri Ojansivu <x@xet7.org>
2022-12-19 02:56:29 +0200

    Fix typos.

--- Source/menu.sh
+++ Source/menu.sh
@@ -24,10 +24,10 @@ function pause(){
         read -p "$*"
 }
 
-echo "Darkest Hour: Build or run with emulator"
+echo "Darkest Hour: Build or run with qemu or other emulator"
 echo
 PS3='Please enter your choice: '
-options=("Build All" "Run Linux-x64" "Run Linux-arm32" "Run Linux-arm64" "Run Linux-ppc" "Run Linux-ppc64" "Run Linux-ppc64le" "Run Linux-riscv64" "Run Linux-s390x" "Quit")
+options=("Build All" "Run Linux-x32" "Run Linux-x64" "Run Linux-arm32" "Run Linux-arm64" "Run Linux-ppc" "Run Linux-ppc64" "Run Linux-ppc64le" "Run Linux-riscv64" "Run Linux-s390x" "Quit")
 
 select opt in "${options[@]}"
 do
@@ -36,35 +36,48 @@ do
       make
       break
       ;;
+    "Run Linux-x32")
+      echo "qemu-i386 ./build/Linux-x32/darkest-hour"
+      qemu-i386 ./build/Linux-x32/darkest-hour
+      break
+      ;;
     "Run Linux-x64")
-      ./build/Linux-x64/darkest-hour
+      echo "qemu-x86_64 ./build/Linux-x64/darkest-hour"
+      qemu-x86_64 ./build/Linux-x64/darkest-hour
       break
       ;;
     "Run Linux-arm32")
+      echo "qemu-arm build/Linux-arm32/darkest-hour"
       qemu-arm build/Linux-arm32/darkest-hour
       break
       ;;
     "Run Linux-arm64")
-      qemu-arm build/Linux-arm32/darkest-hour
+      echo "qemu-aarch64 build/Linux-arm64/darkest-hour"
+      qemu-aarch64 build/Linux-arm64/darkest-hour
       break
       ;;
     "Run Linux-ppc")
+      echo "qemu-ppc build/Linux-ppc/darkest-hour"
       qemu-ppc build/Linux-ppc/darkest-hour
       break
       ;;
     "Run Linux-ppc64")
+      echo "qemu-ppc64 build/Linux-ppc64/darkest-hour"
       qemu-ppc64 build/Linux-ppc64/darkest-hour
       break
       ;;
     "Run Linux-ppc64le")
-      qemu-ppc64le build/Linux-ppcle/darkest-hour
+      echo "qemu-ppc64le build/Linux-ppcle/darkest-hour"
+      qemu-ppc64le build/Linux-ppc64le/darkest-hour
       break
       ;;
     "Run Linux-riscv64")
+      echo "qemu-riscv64 build/Linux-riscv64/darkest-hour"
       qemu-riscv64 build/Linux-riscv64/darkest-hour
       break
       ;;
     "Run Linux-s390x")
+      echo "qemu-s390x build/Linux-s390x/darkest-hour"
       qemu-s390x build/Linux-s390x/darkest-hour
       break
       ;;
