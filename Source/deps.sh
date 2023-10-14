6202d43f022fe3a495edf271f5d9fff930248a1a
Lauri Ojansivu <x@xet7.org>
2023-10-14 19:00:54 -0400

    Removed extra line.

--- Source/deps.sh
+++ Source/deps.sh
@@ -46,7 +46,7 @@ fi
 
 #### Install Linux i386 to AmigaOS3 crosscompiler
 if [ -L /usr/local/bin/68k-amigaos-gcc ] ; then
-	(sudo apt-get -y install gcc-multilib)
+	#(sudo apt-get -y install gcc-multilib)
 	(sudo cp archive/m68k-amigaos_linux_i386.tar.gz /tmp/)
 	(cd /opt && sudo tar zxf /tmp/m68k-amigaos_linux_i386.tar.gz)
 	(cd /usr/local/bin && sudo ln -s /opt/m68k-amigaos/bin/*m68k* . && sudo ln -s /opt/m68k-amigaos/bin/v* .)
